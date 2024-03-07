550a10fa9ce3f9355f0892494ec32263c072cb8b
cos4338@gmail.com <cos4338@gmail.com>
2024-03-07 16:04:01 +0900

    feat: 마이페이지 navigation sidebar 구현

--- src/component/MyPage/common/NavBar.tsx
+++ src/component/MyPage/common/NavBar.tsx
@@ -1,28 +1,44 @@
 'use client';
 
 import {GRAY_COLOR_40, GRAY_COLOR_60, PRIMARY_COLOR} from '@/style/theme';
-import {usePathname} from 'next/navigation';
+import {usePathname, useRouter} from 'next/navigation';
 import styled from 'styled-components';
 
 function NavBar() {
 	const pathname = usePathname();
+	const {push} = useRouter();
 
 	const getIsCurrPage = (input: string) => {
 		return pathname.split('/')[2] === input;
 	};
 
 	return (
-		<StyledAside>
-			<NavItem currpage={getIsCurrPage('user')}>회원정보</NavItem>
-			<NavItem currpage={getIsCurrPage('pet')}>반려동물정보</NavItem>
-			<NavItem currpage={getIsCurrPage('activities')}>활동정보</NavItem>
-		</StyledAside>
+		<Container>
+			<NavItem
+				onClick={() => push('/mypage/user')}
+				currpage={getIsCurrPage('user')}
+			>
+				회원정보
+			</NavItem>
+			<NavItem
+				onClick={() => push('/mypage/pet')}
+				currpage={getIsCurrPage('pet')}
+			>
+				반려동물정보
+			</NavItem>
+			<NavItem
+				onClick={() => push('/mypage/activities')}
+				currpage={getIsCurrPage('activities')}
+			>
+				활동정보
+			</NavItem>
+		</Container>
 	);
 }
 
 export default NavBar;
 
-const StyledAside = styled.aside`
+const Container = styled.aside`
 	display: flex;
 	flex-direction: column;
 	width: 180px;
@@ -31,10 +47,13 @@ const StyledAside = styled.aside`
 	border-right: 2px solid ${GRAY_COLOR_40};
 `;
 
-const NavItem = styled.span<{currpage: boolean}>`
+const NavItem = styled.button<{currpage: boolean}>`
 	padding: 30px 0;
 	font-size: 20px;
 	text-align: left;
+	border: none;
+	background-color: white;
+	cursor: pointer;
 	font-weight: ${(props) => (props.currpage ? 600 : 400)};
 	color: ${(props) => (props.currpage ? PRIMARY_COLOR : GRAY_COLOR_60)};
 `;
